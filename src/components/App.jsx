18b5ca332161123172bbba1d386d6d5ab37200ed
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-06-08 17:04:15 +0300

    feat: add new toy on form submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,10 +17,14 @@ function App() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
